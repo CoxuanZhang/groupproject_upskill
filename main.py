027028eae055a7f351d649e15e53f29518d94304
“CoxuanZhang” <cocozhangkx0307@gmail.com>
2026-01-14 09:08:06 -0500

    take input

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 import random
 
+print("We generated a random number:")
 random_number = random.randint(1, 10)
 print("Random number between 1 and 10:", random_number)
 if random_number > 5:
     print("The number is greater than 5.")
+if random_number//2 ==0:
+    print("The number is even.")
+
+yourint = input("I actually don't care about randomness. What's your fav integer?")
